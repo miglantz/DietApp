7761135045d4dd0af7e4cfaa47ec777cd079d14e
Tomasz <t.wisniewski84@gmail.com>
2018-11-25 12:44:32 +0100

    initial commit

---
+++ src/test/java/pl/atm/dietapp/dietapp/DietappApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.atm.dietapp.dietapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DietappApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
